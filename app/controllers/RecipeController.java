f9f8a9f61f75cb961a87f1785f8e3422adc23f39
DESKTOP-PLEBP5T\David <davidmsl1997@gmail.com>
2020-12-21 19:34:47 +0100

    Nuevos endpoints añadidos

--- app/controllers/RecipeController.java
+++ app/controllers/RecipeController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import models.Recipe;
 import play.libs.Json;
 import play.mvc.*;
@@ -126,6 +127,66 @@ public class RecipeController extends Controller {
             result.put("success", false);
             result.put("message", "No se ha elegido ningún factor de búsqueda");
             response = Results.badRequest(result);
+        }
+
+        return response;
+    }
+
+    public Result patchRecipe(Http.Request request) {
+        Result response;
+        int counter = -1;
+        Recipe recipe;
+
+        Form<Recipe> form = formFactory.form(Recipe.class).bindFromRequest(request);
+
+        if (form.hasErrors()) {
+            return Results.status(CONFLICT, form.errorsAsJson());
+        }
+        recipe = form.get();
+        for (int i = 0; i < Recipe.recipesCollection.size(); i++) {
+            if (recipe.getName().equals(Recipe.recipesCollection.get(i).getName())) {
+                counter = i;
+                break;
+            }
+        }
+        if (counter != -1) {
+            boolean typeFinded = false;
+            for(int i = 0; i < Recipe.typesCollection.size(); i++) {
+                if(Recipe.typesCollection.get(i).equals(recipe.getType().toLowerCase())) {
+                    typeFinded = true;
+                    break;
+                }
+            }
+            if(typeFinded) {
+                Recipe.recipesCollection.set(counter, recipe);
+                if (request.accepts("application/xml")) {
+                    Content content = recipePatched.render(recipe);
+                    response = Results.ok(content);
+                } else if (request.accepts("application/json")) {
+                    ObjectNode result = Json.newObject();
+                    result.put("success", true);
+                    result.put("message", "La receta " + recipe.getName() + " ha sido modificada con éxito");
+                    response = Results.ok(result);
+                } else {
+                    ObjectNode result = Json.newObject();
+                    result.put("success", false);
+                    result.put("message", "Formato no soportado");
+                    response = Results.badRequest(result);
+                }
+            } else {
+                ObjectNode result = Json.newObject();
+                result.put("success", false);
+                result.put("message", "Tipo de la receta no permitido, a continuación se muestran los tipos permitidos");
+                for (int i = 0; i < Recipe.typesCollection.size(); i++) {
+                    result.put("tipo " + (i+1), Recipe.typesCollection.get(i));
+                }
+                response = Results.badRequest(result);
+            }
+        } else {
+            ObjectNode result = Json.newObject();
+            result.put("success", false);
+            result.put("message", "La receta " + recipe.getName() + " no existe");
+            response = Results.notFound(result);
         }
 
         return response;
@@ -264,6 +325,117 @@ public class RecipeController extends Controller {
             }
         }
 
+        return response;
+    }
+
+    public Result getTypes(Http.Request request) {
+        Result response;
+        if (request.accepts("application/xml")) {
+            Content content = types.render(Recipe.typesCollection);
+            response = Results.ok(content);
+        } else if (request.accepts("application/json")) {
+            ArrayNode jsonArray = Json.newArray();
+            ObjectNode result = Json.newObject();
+            for(String type : Recipe.typesCollection) {
+                jsonArray.add(type);
+            }
+            result.put("tipos", jsonArray);
+            response = Results.ok(result);
+        } else {
+            ObjectNode result = Json.newObject();
+            result.put("success", false);
+            result.put("message", "Formato no soportado");
+            response = Results.badRequest(result);
+        }
+        return response;
+    }
+
+    public Result createType(Http.Request request) {
+        Result response;
+        boolean typeFinded = false;
+        Optional<String> recipeType = request.queryString("type");
+        if(recipeType.isPresent() && !recipeType.get().equals("")) {
+            String newType = recipeType.get();
+
+            for(int i = 0; i < Recipe.typesCollection.size(); i++) {
+                if(Recipe.typesCollection.get(i).equals(newType)) {
+                    typeFinded = true;
+                    break;
+                }
+            }
+            if(!typeFinded) {
+                Recipe.typesCollection.add(newType);
+                if (request.accepts("application/xml")) {
+                    Content content = typeCreated.render(newType);
+                    response = Results.ok(content);
+                } else if (request.accepts("application/json")) {
+                    ObjectNode result = Json.newObject();
+                    result.put("success", true);
+                    result.put("message", "El tipo " + newType + " ha sido añadido con éxito");
+                    response = Results.ok(result);
+                } else {
+                    ObjectNode result = Json.newObject();
+                    result.put("success", false);
+                    result.put("message", "Formato no soportado");
+                    response = Results.badRequest(result);
+                }
+            } else {
+                ObjectNode result = Json.newObject();
+                result.put("success", false);
+                result.put("message", "Este tipo de receta ya existe");
+                response = Results.badRequest(result);
+            }
+        } else {
+            ObjectNode result = Json.newObject();
+            result.put("success", false);
+            result.put("message", "No ha especificado ningún tipo para añadir");
+            response = Results.badRequest(result);
+        }
+        return response;
+    }
+
+    public Result deleteType(Http.Request request) {
+        Result response;
+        int counter = -1;
+        Optional<String> recipeType = request.queryString("type");
+
+        if(recipeType.isPresent() && !recipeType.get().equals("")) {
+            String newType = recipeType.get();
+
+            for(int i = 0; i < Recipe.typesCollection.size(); i++) {
+                if(Recipe.typesCollection.get(i).equals(newType)) {
+                    counter = i;
+                    break;
+                }
+            }
+            if(counter != -1) {
+                Recipe.typesCollection.remove(counter);
+                if (request.accepts("application/xml")) {
+                    Content content = typeDeleted.render(newType);
+                    response = Results.ok(content);
+                } else if (request.accepts("application/json")) {
+                    ObjectNode result = Json.newObject();
+                    result.put("success", true);
+                    result.put("message", "El tipo " + newType + " ha sido eliminado con éxito");
+                    response = Results.ok(result);
+                } else {
+                    ObjectNode result = Json.newObject();
+                    result.put("success", false);
+                    result.put("message", "Formato no soportado");
+                    response = Results.badRequest(result);
+                }
+            } else {
+                ObjectNode result = Json.newObject();
+                result.put("success", false);
+                result.put("message", "El tipo expecificado no existe");
+                response = Results.badRequest(result);
+            }
+        } else {
+            ObjectNode result = Json.newObject();
+            result.put("success", false);
+            result.put("message", "No ha especificado ningún tipo para eliminar");
+            response = Results.badRequest(result);
+        }
         return response;
     }
 }
