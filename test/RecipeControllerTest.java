2126711b68d03b5f5871538f6107f88f60bb2ce3
David <davidmsl1997@usal.es>
2021-01-21 18:45:35 +0100

    Test funcionales - RecipeController

--- test/RecipeControllerTest.java
+++ test/RecipeControllerTest.java
@@ -56,4 +56,42 @@ public class RecipeControllerTest extends WithApplication {
         Result r = Helpers.route(app, req);
         Assert.assertEquals(200, r.status());
     }
+
+    @Test
+    public void patchRecipeTest() {
+        ObjectNode body = Json.newObject();
+        body.put("name", "recetaTest25");
+        body.put("type", "Charra");
+        body.put("time", "50");
+        body.put("difficulty", "hard");
+
+        ArrayNode ingredients = Json.newArray();
+        ObjectNode ingredient1 = Json.newObject();
+        ingredient1.put("name", "ternera");
+        ingredient1.put("type", "Carnes");
+        ingredients.add(ingredient1);
+        body.put("ingredients", ingredients);
+
+        ArrayNode allergens = Json.newArray();
+        ObjectNode allergen1 = Json.newObject();
+        allergen1.put("name", "mostaza");
+        allergen1.put("diseases", "Alergia a la mostaza");
+        allergens.add(allergen1);
+        body.put("allergens", allergens);
+
+        ArrayNode steps = Json.newArray();
+        ObjectNode step1 = Json.newObject();
+        step1.put("name", "Paso 1");
+        step1.put("description", "Cocinar");
+        steps.add(step1);
+        body.put("steps", steps);
+
+        Http.RequestBuilder req = Helpers.fakeRequest()
+                .method("PATCH")
+                .uri("http://localhost:9000/recipe")
+                .header("Accept", "application/json")
+                .bodyJson(body);
+        Result r = Helpers.route(app, req);
+        Assert.assertEquals(200, r.status());
+    }
 }
